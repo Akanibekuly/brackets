5d767cf717a9ecec8585e1ee091ef14b37d8b900
Akanibekuly <a.kanibekuly@gmail.com>
2020-09-17 13:20:42 +0600

    feat: brackets problem at file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsMap=new Map(bracketsConfig);
+  console.log(bracketsMap)
+  for(var i=0;i<str.length;i++){
+    if(i==str.length-1){
+      return false;
+    }
+    if(bracketsMap.get(str.charAt(i))){
+      if(bracketsMap.get(str.charAt(i))==str.charAt(i+1)){
+        str=str.substr(0,i)+str.substr(i+2)
+        // console.log(str+'\n')
+        return check(str,bracketsConfig);
+      }
+    } else{
+      return false;
+    }
+  }
+
+  if (str==''){
+    return true;
+  }
+  return false;
 }
